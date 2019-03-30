d805404e07e028540aff5b1b581a2667a8c58217
Cefalu Wave <luke.evan.graham@gmail.com>
2019-03-29 21:31:05 -0700

    got images animating on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ function displayDestinationInfo() {
         method: "GET"
     }).then(function(response) {
         var results = response.data;
-        console.log(results);
+        // console.log(results);
 
         results.forEach(function(result) {
             // Creating a div to hold the destination
@@ -30,21 +30,22 @@ function displayDestinationInfo() {
             // Giving the image tag a src attribute of a property pulled off the result item
             staticIMG.attr("src", result.images.fixed_height_still.url);
 
+            // Give the image tag attributes for still, animate and state
+            staticIMG.attr("data-still", result.images.fixed_height_still.url);
+            staticIMG.attr("data-animate", result.images.fixed_height.url);
+            staticIMG.attr("data-state", "still");
+            staticIMG.attr("class", "gif");
+
             // Appending the paragraph and staticIMG we created to the "destDiv" div we created
             destDiv.append(staticIMG)
             destDiv.append(p);
 
             // Prepending the destDiv to the "#destinations" div in the HTML
             $("#destinations").prepend(destDiv);
-
         })
 
         // var destinationDiv = $("<div class='destination>");
-
-
     });
-
-
 }
 
 
@@ -71,15 +72,37 @@ function renderButtons() {
         // Adding the button to the destination-button div
         $('#destination-buttons').append(button)
     });
+    // console.log(destinations);
 
+}
+
+// Function to switch gif state between animated and still
+function switchGifState() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 }
 
 // // This .on("click") function will trigger the AJAX Call
 // $("addDestination").on("click", function(event) {
 // event.preventDefault();
 
+
+// Function to add destination into array and create button
+function addDestination() {
+    var destination = $("#destination-input").val();
+    destinations.push(destination);
+    renderButtons();
+    console.log(destinations);
+}
+
 // // Grab the text from the input box
-// var destination = $("#destination-input").val();
 
 // // Now construct the query URL
 // var queryURL = 
@@ -88,6 +111,12 @@ function renderButtons() {
 
 // Adding a click event listener to all elements with a class of "dest-btn"
 $(document).on("click", ".dest-btn", displayDestinationInfo);
+
+// Adding a click event listener to all elements  with a class of "gif"
+$(document).on("click", ".gif", switchGifState);
+
+// Adding a click event listener to the "Submit" button
+$(document).on("click", "#addDestination", addDestination);
 
 
 // Calling the renderButtons fuction to display the inistial list of movies

d7d6fa08b11be7d23cdcf40f30727fd8ad19e9e1
Cefalu Wave <luke.evan.graham@gmail.com>
2020-01-25 23:19:59 -0800

    working on favorites displaying

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,7 +25,7 @@ let offset = 0;
 let lastSelectedDestination;
 var appendOrHTML = "html";
 let wikipediaResult;
-let favorites
+let favorites;
 
 function add10() {
   appendOrHTML = "append";
@@ -33,26 +33,29 @@ function add10() {
   displayDestinationInfo(offset);
 }
 
+function favoriteClick(gifFaved) {
+  console.log("FAVE: ", gifFaved);
+  $("#favorites").addClass(`p-3`)
+  $("#favorites").append(`<div class="col-4 p-0"><img src="` + gifFaved + `" alt="" class="img-fluid"></div>`)
+}
+
 function getWikipedia(chosenDest) {
+  $.ajax({
+    url:
+      `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&titles=` +
+      chosenDest +
+      `&prop=pageterms|pageprops|pageimages|pageterms|extracts`,
+    method: "GET"
+  }).then(wikiResponse => {
+    const wikiPageNum = Object.keys(wikiResponse.query.pages);
+    wikiData = wikiResponse.query.pages[wikiPageNum];
 
-    $.ajax({
-      url:
-        `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&titles=` +
-        chosenDest +
-        `&prop=pageterms|pageprops|pageimages|pageterms|extracts`,
-      method: "GET"
-    }).then(wikiResponse => {
-      const wikiPageNum = Object.keys(wikiResponse.query.pages)
-      wikiData = wikiResponse.query.pages[wikiPageNum]
-
-      $("#wikipedia-form").empty()
-      $("#wikipedia-form").addClass(`myClass`)
-      $("#wikipedia-form").append(`<h5>` + wikiData.title + `</h5>`)
-      // $("#wikipedia-form").append(`<p class='description'>` + wikiData.terms.description + `</p>`)
-      let shortenedExtract = wikiData.extract
-      // console.log("HEY: ", shortenedExtract.split(`</p>`, 3))
-      $("#wikipedia-form").append(shortenedExtract.split(`</p>`, 3))
-  })
+    $("#wikipedia-form").empty();
+    $("#wikipedia-form").addClass(`myClass`);
+    $("#wikipedia-form").append(`<h5>` + wikiData.title + `</h5>`);
+    let shortenedExtract = wikiData.extract;
+    $("#wikipedia-form").append(shortenedExtract.split(`</p>`, 3));
+  });
 }
 
 // displayDestinationInfo function re-renders the HTML to diplay the appropriate content
@@ -69,7 +72,7 @@ function displayDestinationInfo(currentOffset) {
     lastSelectedDestination = $(this).attr("data-name");
     chosenDestination = $(this).attr("data-name");
     $(".add10Button").remove();
-    getWikipedia($(this).attr("data-name"))
+    getWikipedia($(this).attr("data-name"));
   }
 
   var queryURL =
@@ -77,24 +80,6 @@ function displayDestinationInfo(currentOffset) {
     chosenDestination +
     "&api_key=6NPx7xPqPCxdQFVMsaIiTbtvP0EpnX8k&limit=10&offset=" +
     currentOffset;
-
-  // Promise.all([
-  //   $.ajax({
-  //     url: queryURL,
-  //     method: "GET"
-  //   }),
-  //   $.ajax({
-  //     url:
-  //       `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&titles=` +
-  //       chosenDestination +
-  //       `&prop=extracts|pageterms|pageprops|pageimages|pageterms`,
-  //     method: "GET"
-  //   })
-  // ]).then(resultArray => {
-  //   console.log("RESULT: ", resultArray)
-  //   giphyResult = resultArray[0];
-  //   wikipediaResult = resultArray[1];
-  // });
 
   // // Creating an AJAX call for the specific destination button being clicked
   $.ajax({
@@ -104,23 +89,20 @@ function displayDestinationInfo(currentOffset) {
     var results = response.data;
 
     results.forEach(function(result) {
-
       // Creating a div to hold the destination
-      var destDiv = $("<div class='destination p-3'>");
+      var destDiv = $("<div class='destination row'>");
 
       // Storing the rating image of gif
       var rating = result.rating;
 
       // Creating a paragraph tag with the result item's rating
-      var p = $("<p class='below-img'>").text("Rating: " + rating);
+      var p = $("<div class='col-8 pb-4'>").text("Rating: " + rating);
 
-      p.prepend(
-        `<div class='break'></div>` +
-          result.title.toUpperCase() +
-          ` <a href="#" onclick="download_img(this, '` +
-          result.images.fixed_height.url +
-          `')"><button class="btn btn-dark download-btn"><i class="fas fa-download"></i></button></a><br>`
-      );
+      var icons = $(`<div class='col-4 d-flex flex-row-reverse'>`).text("");
+
+      p.prepend(result.title.toUpperCase() + ` <br>`);
+
+      let divCol = $(`<div class='col-12'>`);
 
       // Creating an image tag
       var staticIMG = $("<img>");
@@ -134,12 +116,24 @@ function displayDestinationInfo(currentOffset) {
       staticIMG.attr("data-state", "still");
       staticIMG.attr("class", "gif rounded img-fluid");
 
+      icons.append(
+        `<button class="btn btn-dark download-btn mr-0" onclick="favoriteClick('` + result.images.fixed_width_small_still.url + `')"><i class="fas fa-star"></i></button>
+      <a href="#" onclick="download_img(this, '` +
+          result.images.fixed_height.url +
+          `')"><button class="btn btn-dark download-btn"><i class="fas fa-download"></i></button></a>`
+      );
+
       // Appending the paragraph and staticIMG we created to the "destDiv" div we created
-      destDiv.append(staticIMG);
+      staticIMG.appendTo(divCol);
+      destDiv.append(divCol);
+      // destDiv.append(`<div class='row'>`)
+      // destDiv.append(`<div class='row'>`)
       destDiv.append(p);
+      destDiv.append(icons);
 
       // Prepending the destDiv to the "#destinations" div in the HTML
       //   if (appendOrHTML === "append") {
+      $("#destinations").addClass("p-3");
       $("#destinations").append(destDiv);
       //   } else {
       // $("#destinations").html(destDiv);
@@ -147,7 +141,7 @@ function displayDestinationInfo(currentOffset) {
     });
 
     $("#destinations").append(
-      `<button class="add10Button btn btn-dark m-3">Add 10 more`
+      `<button class="add10Button btn btn-dark">Add 10 more`
     );
 
     $(".add10Button").on("click", add10);
@@ -190,7 +184,6 @@ function switchGifState() {
 }
 
 // // This .on("click") function will trigger the AJAX Call
-// $("addDestination").on("click", function(event) {
 
 // Function to add destination into array and create button
 function addDestination() {
@@ -201,8 +194,6 @@ function addDestination() {
 }
 
 function download_img(e, link) {
-  console.log("LINK: ", link);
-
   var image = new Image();
   image.crossOrigin = "anonymous";
   image.src = link;
